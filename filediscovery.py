8534a5b34697bc2dc3f20719c729cf94d0c511f9
Joel-O1 <joelthomas0324@gmail.com>
2026-03-10 19:10:06 -0600

    Save current progress

---
+++ filediscovery.py
@@ -0,0 +1,39 @@
+import os
+
+TARGET_EXTENSIONS = (
+    ".doc", ".docx",
+    ".pdf",
+    ".xls", ".xlsx",
+    ".ppt", ".pptx"
+)
+
+USER_HOME = os.environ.get("USERPROFILE")
+
+TARGET_FOLDERS = [
+    os.path.join(USER_HOME, "Documents"),
+    os.path.join(USER_HOME, "Downloads"),
+    os.path.join(USER_HOME, "Pictures"),
+    os.path.join(USER_HOME, "Videos")
+]
+
+
+def scan_files():
+    found_files = []
+
+    for folder in TARGET_FOLDERS:
+        if not os.path.exists(folder):
+            continue
+
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file.lower().endswith(TARGET_EXTENSIONS):
+
+                    file_name = file
+                    file_path = os.path.join(root, file)
+
+                    found_files.append({
+                        "name": file_name,
+                        "path": file_path
+                    })
+
+    return found_files
